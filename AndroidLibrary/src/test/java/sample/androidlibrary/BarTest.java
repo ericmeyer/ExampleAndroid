f25f767cc9efc57219a9afed1a589c0b7842169b
Eric Meyer <emeyer@8thlight.com>
2014-12-17 21:39:51 -0800

    emulateSdk 18 for BarTest

--- AndroidLibrary/src/test/java/sample/androidlibrary/BarTest.java
+++ AndroidLibrary/src/test/java/sample/androidlibrary/BarTest.java
@@ -3,11 +3,13 @@ package sample.androidlibrary;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class BarTest {
 
